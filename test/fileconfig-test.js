1d0a7fbb9099790ebab49d261f127d9d92095f28
Hasnae Rehioui <hrehioui@atlassian.com>
2016-01-12 14:35:35 +1100

    #4 atvalue to return a frozen/immutable copy

--- test/fileconfig-test.js
+++ test/fileconfig-test.js
@@ -17,6 +17,20 @@ describe('fileconfig-json', () => {
         it('should resolve environment variables', () => {
             assert.equal(process.env.SERVER_NAME, TestConfig.servers.jsonTest.name);
         });
+        it('should return an immutable copy of component["@value"]', () => {
+            let server = TestConfig.servers.jsonTest['@value'];
+            assert.equal(8080, server.port);
+            assert.throws(() => {
+                server.port = 9090;
+            }, TypeError);
+            assert.equal(8080, server.port);
+            assert.equal(8080, TestConfig.servers.jsonTest.port);
+        });
+        it('should be read only', () => {
+           let server = TestConfig.servers.jsonTest;
+            server.port = 9090;
+            assert.equal(8080, server.port);
+        });
     });
 });
 
@@ -28,6 +42,20 @@ describe('fileconfig-yml', () => {
         });
         it('should resolve environment variables', () => {
             assert.equal(process.env.SERVER_NAME, TestConfig.servers.ymlTest.name);
-        })
+        });
+        it('should return an immutable copy of component["@value"]', () => {
+            let server = TestConfig.servers.ymlTest['@value'];
+            assert.equal(9090, server.port);
+            assert.throws(() => {
+                server.port = 8080;
+            }, TypeError);
+            assert.equal(9090, server.port);
+            assert.equal(9090, TestConfig.servers.ymlTest.port);
+        });
+        it('should be read only', () => {
+            let server = TestConfig.servers.ymlTest;
+            server.port = 8080;
+            assert.equal(9090, server.port);
+        });
     });
 });
