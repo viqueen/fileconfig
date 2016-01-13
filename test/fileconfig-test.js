93da0f87d3378cc9bdb77d9b8471155d38683d33
Hasnae Rehioui <hrehioui@atlassian.com>
2016-01-13 20:30:08 +1100

    #4 ensure component at.path property is accessible

--- test/fileconfig-test.js
+++ test/fileconfig-test.js
@@ -8,54 +8,68 @@ process.env.SERVER_NAME = 'DEFAULT SERVER';
 let FileConfig = require('../lib/fileconfig');
 let TestConfig = new FileConfig(path.resolve(__dirname, 'config'));
 
+describe('fileconfig-dir', () => {
+    it('should return component @path property', () => {
+        let servers = TestConfig.servers;
+        let cpath   = path.resolve(__dirname, 'config/servers');
+        assert.equal(cpath, servers['@path']);
+    });
+});
+
 describe('fileconfig-json', () => {
-    describe('#get', () => {
-        it('should return data in jsonTest.json', () => {
-            assert.equal(8080, TestConfig.servers.jsonTest.port);
-            assert.equal(8080, TestConfig['servers/jsonTest/port']);
-        });
-        it('should resolve environment variables', () => {
-            assert.equal(process.env.SERVER_NAME, TestConfig.servers.jsonTest.name);
-        });
-        it('should return an immutable copy of component["@value"]', () => {
-            let server = TestConfig.servers.jsonTest['@value'];
-            assert.equal(8080, server.port);
-            assert.throws(() => {
-                server.port = 9090;
-            }, TypeError);
-            assert.equal(8080, server.port);
-            assert.equal(8080, TestConfig.servers.jsonTest.port);
-        });
-        it('should be read only', () => {
-           let server = TestConfig.servers.jsonTest;
+    it('should return data in jsonTest.json', () => {
+        assert.equal(8080, TestConfig.servers.jsonTest.port);
+        assert.equal(8080, TestConfig['servers/jsonTest/port']);
+    });
+    it('should resolve environment variables', () => {
+        assert.equal(process.env.SERVER_NAME, TestConfig.servers.jsonTest.name);
+    });
+    it('should return an immutable copy of component["@value"]', () => {
+        let server = TestConfig.servers.jsonTest['@value'];
+        assert.equal(8080, server.port);
+        assert.throws(() => {
             server.port = 9090;
-            assert.equal(8080, server.port);
-        });
+        }, TypeError);
+        assert.equal(8080, server.port);
+        assert.equal(8080, TestConfig.servers.jsonTest.port);
+    });
+    it('should be read only', () => {
+       let server = TestConfig.servers.jsonTest;
+        server.port = 9090;
+        assert.equal(8080, server.port);
+    });
+    it('should return component @path property', () => {
+        let server = TestConfig.servers.jsonTest;
+        let cpath  = path.resolve(__dirname, 'config/servers/jsonTest.json');
+        assert.equal(cpath, server['@path']);
     });
 });
 
 describe('fileconfig-yml', () => {
-    describe('#get', () => {
-        it('should return data in ymlTest.yml', () => {
-            assert.equal(9090, TestConfig.servers.ymlTest.port);
-            assert.equal(9090, TestConfig['servers/ymlTest/port']);
-        });
-        it('should resolve environment variables', () => {
-            assert.equal(process.env.SERVER_NAME, TestConfig.servers.ymlTest.name);
-        });
-        it('should return an immutable copy of component["@value"]', () => {
-            let server = TestConfig.servers.ymlTest['@value'];
-            assert.equal(9090, server.port);
-            assert.throws(() => {
-                server.port = 8080;
-            }, TypeError);
-            assert.equal(9090, server.port);
-            assert.equal(9090, TestConfig.servers.ymlTest.port);
-        });
-        it('should be read only', () => {
-            let server = TestConfig.servers.ymlTest;
+    it('should return data in ymlTest.yml', () => {
+        assert.equal(9090, TestConfig.servers.ymlTest.port);
+        assert.equal(9090, TestConfig['servers/ymlTest/port']);
+    });
+    it('should resolve environment variables', () => {
+        assert.equal(process.env.SERVER_NAME, TestConfig.servers.ymlTest.name);
+    });
+    it('should return an immutable copy of component["@value"]', () => {
+        let server = TestConfig.servers.ymlTest['@value'];
+        assert.equal(9090, server.port);
+        assert.throws(() => {
             server.port = 8080;
-            assert.equal(9090, server.port);
-        });
+        }, TypeError);
+        assert.equal(9090, server.port);
+        assert.equal(9090, TestConfig.servers.ymlTest.port);
+    });
+    it('should be read only', () => {
+        let server = TestConfig.servers.ymlTest;
+        server.port = 8080;
+        assert.equal(9090, server.port);
+    });
+    it('should return component @path property', () => {
+        let server = TestConfig.servers.ymlTest;
+        let cpath  = path.resolve(__dirname, 'config/servers/ymlTest.yml');
+        assert.equal(cpath, server['@path']);
     });
 });
